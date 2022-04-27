75d367e613df8d502d32079157a6d96e8c5fa3fb
Nadia Khoptiuk <holineinadia@gmail.com>
2022-04-27 20:28:45 +0300

    Add script for fixed body when modal windows are opened

--- src/js/contacts-modal.js
+++ src/js/contacts-modal.js
@@ -1,14 +1,15 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector(".contacts-btn__location"),
-    closeMenuBtn: document.querySelector(".map__btn"),
-    menu: document.querySelector(".contacts__map"),
+    openMenuBtn: document.querySelector('.contacts-btn__location'),
+    closeMenuBtn: document.querySelector('.map__btn'),
+    menu: document.querySelector('.contacts__map'),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
+    refs.menu.classList.toggle('is-hidden');
+    document.body.classList.toggle('map-open');
   }
 })();
