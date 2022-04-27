9ee524cbfc5206bdf7cefeb13d1f335097c05575
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-04-27 16:13:04 +0300

    add js

---
+++ src/js/contacts-modal.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector(".contacts-btn__location"),
+    closeMenuBtn: document.querySelector(".map__btn"),
+    menu: document.querySelector(".contacts__map"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+  }
+})();
