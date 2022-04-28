61394f0dae1964e00b83d68f3eba71eec2b55ac5
Maksym Shumeiko <kkiilleerraa@mail.ru>
2022-04-28 10:07:12 +0300

    menu-fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,14 +5,19 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
 
     modal: document.querySelector('[data-modal]'),
+    modalClo: document.querySelector('[data-modal-clo]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.openModalBtn2.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal2);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
     document.body.classList.toggle('modal-open');
+  }
+  function toggleModal2() {
+    refs.modalClo.classList.toggle('is-open');
   }
 })();
