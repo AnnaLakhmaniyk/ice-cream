fd5f079aee4ac127f6578b4390afe15eb232af5f
Maksym Shumeiko <kkiilleerraa@mail.ru>
2022-04-28 09:37:16 +0300

    add img and hero modal

--- src/js/about.js
+++ src/js/about.js
@@ -1,10 +1,12 @@
 (() => {
     const refs = {
-         openModalAboutBtn: document.querySelector("[data-about-open]"),
+        openModalAboutBtn: document.querySelector("[data-about-open]"),
+        openModalAboutBtn2: document.querySelector("[data-about-open2]"),
         closeModalAboutBtn: document.querySelector("[data-about-close]"),
         modalAbout: document.querySelector("[data-about]"),
     };
     refs.openModalAboutBtn.addEventListener("click", toggleModalAbout);
+    refs.openModalAboutBtn2.addEventListener("click", toggleModalAbout);
     refs.closeModalAboutBtn.addEventListener("click", toggleModalAbout);
     
     function toggleModalAbout() {
