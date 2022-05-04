00467068b9ef7658619f5236b74ae13968fc8bc7
MaxKorolko <m.v.korolko@gmail.com>
2022-05-04 17:53:39 +0300

    js/calc

--- src/js/basket.js
+++ src/js/basket.js
@@ -1,29 +1,42 @@
 window.addEventListener('click', function (event) {
+  const basketItems = document.querySelectorAll('.basket__item');
+
+  let totalPrice = 0;
+
+  basketItems.forEach(function (item) {
+    const amountEl = item.querySelector('[data-quantity]');
+    const priceEl = item.querySelector('#current-price');
+
+    const currentPrice = Number(parseInt(amountEl.innerText)) * Number(parseInt(priceEl.innerText));
+
+    totalPrice += currentPrice;
+  });
+
+  document.querySelector('#total-price').textContent = totalPrice;
+
+  let quantity;
+
+  if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
+    const basketBtnWrap = event.target.closest('.basket__btn-wrap');
+
+    quantity = basketBtnWrap.querySelector('[data-quantity]');
+  }
+
   // пошук кнопки плюс
   if (event.target.dataset.action === 'plus') {
-    console.log('Plus');
-
-    const basketBtnWrap = event.target.closest('.basket__btn-wrap');
-    console.log(basketBtnWrap);
-
-    const quantity = basketBtnWrap.querySelector('[data-quantity]');
-    console.log(quantity);
-
     quantity.innerText = ++quantity.innerText;
   }
 
   // пошук кнопки мінус
   if (event.target.dataset.action === 'minus') {
-    console.log('Minus');
-
-    const basketBtnWrap = event.target.closest('.basket__btn-wrap');
-    console.log(basketBtnWrap);
-
-    const quantity = basketBtnWrap.querySelector('[data-quantity]');
-    console.log(quantity);
-
     if (parseInt(quantity.innerText) > 0) {
       quantity.innerText = --quantity.innerText;
     }
   }
 });
+
+// target
+
+// onclick
+
+// event
